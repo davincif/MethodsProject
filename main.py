ff7ac83dfb28680bc26fe8e64fcba6f69e5199b9
davincif <ldavincif@gmail.com>
2018-04-20 19:00:12 -0300

    adams done + get user expression

--- main.py
+++ main.py
@@ -5,37 +5,48 @@ try:
 except Exception as exc:
 	print("you must have python version 3.5 or higher.\nCurrent is: " + str(sys.version))
 	exit()
-from pprint import pprint
+import json
 
 from sympy.core import sympify
-from sympy import lambdify
 
-dydx = None
-Y = None
-X = None
+dydt = None #y'
+Y = None #y
+T = None #t
 
-#x + 1 + y**3 - 2*x
 def main():
-	global dydx
+	global dydt
 	global mvar
+	global Y
+	global T
+
+	qtdvar = 0
 
 	#read function
 	while True:
 		try :
 			Y = sympify('y')
+			T = sympify('t')
 			print("dy/dx = ", end="")
-			dydx = sympify(input(), evaluate=True)
-			if (len(dydx.atoms()) > 2):
-				raise("There must be 2 variables at maximum")
-			for symb in dydx.atoms():
+			dydt = sympify(input(), evaluate=True)
+
+			for symb in dydt.atoms():
 				if(symb == Y):
 					Y = symb
-				else:
-					X = symb
-			print('Expression: ' + str(dydx))
+				elif(symb == T):
+					T = symb
+
+				if(type(symb) == type(Y)):
+					qtdvar += 1
+
+			# 1-t+4*y
+			if (qtdvar < 0 or qtdvar > 2):
+				raise Exception("There must be >= 0 and <= 2 variables at maximum: this expretion has " + str(qtdvar))
+
+			print('Expression: ' + str(dydt))
 
 			break
-		except ValueError as exp:
+		except Exception as exp:
+		# except ValueError as exp:
 			print(exp, end='\n\n')
 			print("Try again")
 
@@ -45,20 +56,28 @@ def main():
 	methods['eulerBackward'] = eulerBackward(6, 0.05, 0, 1.0, funczinha)
 	methods['eulerMod'] = eulerMod(16, 0.025, 0, 1.0, funczinha)
 	methods['rungeKutta'] = rungeKutta(16, 0.025, 0, 1.0, funczinha)
-	# methods['adamBashford'] = adamBashford(10, 0.025, 0, 1.0, funczinha)
-	pprint(methods)
+	methods['adamBashford_2'] = adamBashford_2(16, 0.025, 0, 1.0, funczinha, rungeKutta)
+	methods['adamBashford_3'] = adamBashford_3(16, 0.025, 0, 1.0, funczinha, rungeKutta)
+	methods['adamBashford_4'] = adamBashford_4(16, 0.025, 0, 1.0, funczinha, rungeKutta)
+	print(json.dumps(methods, indent=4)) #sort_keys=True
 
 def funczinha(t, y):
-	# aux = aux.subs({mvar[0]: hn, mvar[1]: }).evalf()
-	return 1 - t + 4*y
+	global dydt
+	global mvar
+	global Y
+	global T
+
+	#1 - t + 4*y
+	aux = dydt.subs({Y: y, T: t}).evalf()
+	return aux
 
 def euler(qtd, h, hi, yhi, func):
 	yn = yhi
 	tn = hi
 	table = []
 	while(qtd > 0):
-		table.append([tn, yn])
-		yn = yn + h*func(tn, yn)
+		table.append([tn, float(yn)])
+		yn = (yn + h*func(tn, yn)).evalf()
 		tn += h
 		qtd -= 1
 
@@ -70,7 +89,7 @@ def eulerBackward(qtd, h, hi, yhi, func):
 	tn = hi
 	table = []
 	while(qtd > 0):
-		table.append([tn, yn])
+		table.append([tn, float(yn)])
 		byn = yn + h*func(tn, yn)
 		yn = yn + h*func(tn+h, byn)
 		tn += h
@@ -83,7 +102,7 @@ def eulerMod(qtd, h, hi, yhi, func):
 	tn = hi
 	table = []
 	while(qtd > 0):
-		table.append([tn, yn])
+		table.append([tn, float(yn)])
 		byn = yn + h*func(tn, yn)
 		yn = yn + 0.5*h*(func(tn, yn) + func(tn+h, byn))
 		tn += h
@@ -97,7 +116,7 @@ def rungeKutta(qtd, h, hi, yhi, func):
 	k1 = k2 = k3 = k4 = None
 	table = []
 	while(qtd > 0):
-		table.append([tn, yn])
+		table.append([tn, float(yn)])
 		k1 = func(tn, yn)
 		k2 = func(tn + 0.5*h, yn + 0.5*h*k1)
 		k3 = func(tn + 0.5*h, yn + 0.5*h*k2)
@@ -108,13 +127,66 @@ def rungeKutta(qtd, h, hi, yhi, func):
 
 	return table
 
-def adamBashford(qtd, h, hi, yhi, func):
+def adamBashford_2(qtd, h, hi, yhi, func, meth):
+	lastp = [x[1] for x in meth(2, h, hi, yhi, func)] #lastp == last points
 	yn = yhi
-	tn = hitable = []
+	tn0 = hi
+	tn1 = hi+h
+	table = [[tn0, lastp[0]], [tn1, lastp[1]]]
+	qtd -= 2
 	while(qtd > 0):
-		print(tn, "|", yn)
-		tn += h
+		yn = lastp[1] + 0.5*h * (3*func(tn1, lastp[1]) - func(tn0, lastp[0]))
+		lastp[0] = lastp[1]
+		lastp[1] = yn
+		tn0 = tn1
+		tn1 += h
 		qtd -= 1
+		table.append([tn1, float(yn)])
+
+	return table
+
+def adamBashford_3(qtd, h, hi, yhi, func, meth):
+	lastp = [x[1] for x in meth(3, h, hi, yhi, func)] #lastp == last points
+	yn = yhi
+	tn0 = hi
+	tn1 = hi+h
+	tn2 = tn1+h
+	table = [[tn0, lastp[0]], [tn1, lastp[1]], [tn2, lastp[2]]]
+	qtd -= 3
+	while(qtd > 0):
+		yn = lastp[2] + h*(23*func(tn2, lastp[2]) - 16*func(tn1, lastp[1]) + 5*func(tn0, lastp[0]))/12
+		lastp[0] = lastp[1]
+		lastp[1] = lastp[2]
+		lastp[2] = yn
+		tn0 = tn1
+		tn1 = tn2
+		tn2 += h
+		qtd -= 1
+		table.append([tn2, float(yn)])
+
+	return table
+
+def adamBashford_4(qtd, h, hi, yhi, func, meth):
+	lastp = [x[1] for x in meth(4, h, hi, yhi, func)] #lastp == last points
+	yn = yhi
+	tn0 = hi
+	tn1 = hi+h
+	tn2 = tn1+h
+	tn3 = tn2+h
+	table = [[tn0, lastp[0]], [tn1, lastp[1]], [tn2, lastp[2]], [tn3, lastp[3]]]
+	qtd -= 4
+	while(qtd > 0):
+		yn = lastp[3] + h*(55*func(tn3, lastp[3]) - 59*func(tn2, lastp[2]) + 37*func(tn1, lastp[1]) - 9*func(tn0, lastp[0]))/24
+		lastp[0] = lastp[1]
+		lastp[1] = lastp[2]
+		lastp[2] = lastp[3]
+		lastp[3] = yn
+		tn0 = tn1
+		tn1 = tn2
+		tn2 = tn3
+		tn3 += h
+		qtd -= 1
+		table.append([tn3, float(yn)])
 
 	return table
 
