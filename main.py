7fc9397e7aa877b02870dc150f5cf25012f8bad9
davincif <LDaVinciF@gmail.com>
2018-04-20 12:01:11 -0300

    better print and py version check

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#python3
+import sys
+try:
+	assert sys.version_info >= (3,5)
+except Exception as exc:
+	print("you must have python version 3.5 or higher.\nCurrent is: " + str(sys.version))
+	exit()
+from pprint import pprint
+
 from sympy.core import sympify
 from sympy import lambdify
 
@@ -32,10 +41,12 @@ def main():
 
 	#run methods
 	methods = {}
-	methods['euler'] = euler(6, 0.05, 0, 1, funczinha)
-	methods['eulerBackward'] = eulerBackward(6, 0.05, 0, 1, funczinha)
-	methods['eulerMod'] = eulerMod(16, 0.025, 0, 1, funczinha)
-	print(methods)
+	methods['euler'] = euler(6, 0.05, 0, 1.0, funczinha)
+	methods['eulerBackward'] = eulerBackward(6, 0.05, 0, 1.0, funczinha)
+	methods['eulerMod'] = eulerMod(16, 0.025, 0, 1.0, funczinha)
+	methods['rungeKutta'] = rungeKutta(16, 0.025, 0, 1.0, funczinha)
+	# methods['adamBashford'] = adamBashford(10, 0.025, 0, 1.0, funczinha)
+	pprint(methods)
 
 def funczinha(t, y):
 	# aux = aux.subs({mvar[0]: hn, mvar[1]: }).evalf()
@@ -44,41 +55,68 @@ def funczinha(t, y):
 def euler(qtd, h, hi, yhi, func):
 	yn = yhi
 	tn = hi
+	table = []
 	while(qtd > 0):
+		table.append([tn, yn])
 		yn = yn + h*func(tn, yn)
 		tn += h
 		qtd -= 1
 
-	return yn
+	return table
 
 def eulerBackward(qtd, h, hi, yhi, func):
+	h = float(h)
 	yn = yhi
 	tn = hi
+	table = []
 	while(qtd > 0):
-		print(tn, "|", yn)
+		table.append([tn, yn])
 		byn = yn + h*func(tn, yn)
 		yn = yn + h*func(tn+h, byn)
 		tn += h
 		qtd -= 1
 
-	return yn
+	return table
 
 def eulerMod(qtd, h, hi, yhi, func):
 	yn = yhi
 	tn = hi
+	table = []
 	while(qtd > 0):
+		table.append([tn, yn])
 		byn = yn + h*func(tn, yn)
-		yn = yn + (h/2)*(func(tn, yn) + func(tn+h, byn))
+		yn = yn + 0.5*h*(func(tn, yn) + func(tn+h, byn))
 		tn += h
 		qtd -= 1
 
-	return yn
+	return table
 
-def rungeKutta():
-	pass
+def rungeKutta(qtd, h, hi, yhi, func):
+	yn = yhi
+	tn = hi
+	k1 = k2 = k3 = k4 = None
+	table = []
+	while(qtd > 0):
+		table.append([tn, yn])
+		k1 = func(tn, yn)
+		k2 = func(tn + 0.5*h, yn + 0.5*h*k1)
+		k3 = func(tn + 0.5*h, yn + 0.5*h*k2)
+		k4 = func(tn + h, yn + h*k3)
+		yn = yn + (h/6)*(k1 + 2*(k2+k3) + k4)
+		tn += h
+		qtd -= 1
 
-def adamBashford():
-	pass
+	return table
+
+def adamBashford(qtd, h, hi, yhi, func):
+	yn = yhi
+	tn = hitable = []
+	while(qtd > 0):
+		print(tn, "|", yn)
+		tn += h
+		qtd -= 1
+
+	return table
 
 def adamMoulton():
 	pass
